a79db72667db76c1c8c9cd64b72d104b44dc0dc8
Sayan Basani <sayanbasani1@gmail.com>
2026-04-01 18:07:27 +0530

    it is the init of the project .

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'mydb',
+    'db_user' => 'root',
+    'db_pass' => 'secret123',
+    'api_key' => 'abcd1234',
+    'email'   => 'LabReport@g.com',
+    'phone_number'   => 1234567890,
+    'Address' => 'ABC * DEF * XYZ',
+    'clinic_name' => 'SPD Lab Report'
+];
+?>
